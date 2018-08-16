2aa571a28e3b5af14fe648c2f44758c2099c9560
Anu Khambete <anukhambete@gmail.com>
2018-08-16 02:27:22 +0000

    Hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,12 @@ require './config/environment'
 require 'pry'
 
 class ApplicationController < Sinatra::Base
+  configure do
+      set :views, 'app/views'
+    end
+
+  get '/' do
+    erb :main
+  end
 
 end
