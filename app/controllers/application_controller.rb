c0624d9a115850925e90d9e5034d4b726e930f39
Anu Khambete <anukhambete@gmail.com>
2018-08-16 03:38:11 +0000

    testing views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,17 @@ require './config/environment'
 require 'pry'
 
 class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  enable :sessions
+  set :session_secret, "password_security"
+
   configure do
+      set :public_folder, 'public'
       set :views, 'app/views'
     end
 
   get '/' do
-    erb :main
+    erb :'/users/create_user'
   end
 
 end
